9ee300c0846916a9347f33ba5976931fdc98eef9
TmLololol <tmtanky@gmail.com>
2021-03-04 06:48:54 +0800

    11th commit added new project

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ const link7 = document.querySelector(`.box7`)
 const link8 = document.querySelector(`.box8`)
 const link9 = document.querySelector(`.box9`)
 const link10 = document.querySelector(`.box10`)
+const link11 = document.querySelector(`.box11`)
 
 link1.addEventListener(`click`, function() {
     window.open('https://product-lorem.herokuapp.com/', '_blank');
@@ -47,5 +48,9 @@ link9.addEventListener(`click`, function() {
 
 link10.addEventListener(`click`, function() {
     window.open('https://pokesearch-react.netlify.app/', '_blank');
+})
+
+link11.addEventListener(`click`, function() {
+    window.open('https://oishi-foodhub-react.netlify.app/', '_blank');
 })
 
